9ba71ddca50034f1387a89a99fc942ddb14a41ba
Konduru Sathvik Varma <sathvikvarma1216@gmail.com>
2026-08-29 14:40:56 +0530

    Time: 81 ms (92.79%) | Memory: 28.8 MB (30.62%) - LeetSync

---
+++ Arrays/739-daily-temperatures/daily-temperatures.py
@@ -0,0 +1,17 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # Answer array, initially all 0
+        answer = [0] * len(temperatures)
+        # Stack stores INDEXES of days
+        stack = []
+        for i in range(len(temperatures)):
+            # If today's temperature is warmer,
+            # today's day is the answer for previous colder days
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                # Get the previous day
+                prev = stack.pop()
+                # Number of days we waited
+                answer[prev] = i - prev
+            # Current day is now waiting for a warmer day
+            stack.append(i)
+        return answer
