a306852b888090708f47e955aaf3439de0ea7c63
Konduru Sathvik Varma <sathvikvarma1216@gmail.com>
2026-08-29 16:40:16 +0530

    Time: 31 ms (38.59%) | Memory: 26.2 MB (59.95%) - LeetSync

---
+++ Arrays/2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.py
@@ -0,0 +1,9 @@
+import heapq
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, int(num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return str(heap[0])
