94674bcdd30a10927e5a1b7b38b72de605d91110
Konduru Sathvik Varma <sathvikvarma1216@gmail.com>
2026-08-29 14:42:00 +0530

    Time: 90 ms (68.98%) | Memory: 28.7 MB (46.80%) - LeetSync

--- Arrays/739-daily-temperatures/daily-temperatures.py
+++ Arrays/739-daily-temperatures/daily-temperatures.py
@@ -2,7 +2,7 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         # Answer array, initially all 0
         answer = [0] * len(temperatures)
-        # Stack stores INDEXES of days
+        # Stack stores INDEXES of days not temperatures
         stack = []
         for i in range(len(temperatures)):
             # If today's temperature is warmer,
