d237fc3448b612adde11aa9917796038837990ba
Konduru Sathvik Varma <sathvikvarma1216@gmail.com>
2026-08-29 17:37:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (26.37%) - LeetSync

---
+++ Arrays/31-next-permutation/next-permutation.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
+        i = n - 2
+        # Find the first number from the right that is smaller than the next number
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        # If such a number exists, find a bigger number from the right
+        if i >= 0:
+            j = n - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            # Swap them to make the permutation slightly bigger
+            nums[i], nums[j] = nums[j], nums[i]
+        # Reverse the right side to make it as small as possible
+        nums[i + 1:] = reversed(nums[i + 1:])
