5ec1b1382b2049edf111f8fa0e313e34388fbae0
Konduru Sathvik Varma <sathvikvarma1216@gmail.com>
2026-09-20 12:35:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (69.25%) - LeetSync

---
+++ BackTracking/78-subsets/subsets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        ans, sol = [], []  # Store all subsets and build one subset
+        n = len(nums)
+
+        def backtrack(start):
+            ans.append(sol[:])  # Save the current subset
+
+            for i in range(start, n):  # Try each remaining number
+                sol.append(nums[i])  # Choose the number
+                backtrack(i + 1)  # Continue with the next index
+                sol.pop()  # Undo the choice
+
+        backtrack(0)  # Start from index 0
+        return ans  # Return all subsets
