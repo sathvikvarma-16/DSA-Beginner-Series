2d271c052b71fa3efca1024be210dfd1032eb8f5
Konduru Sathvik Varma <sathvikvarma1216@gmail.com>
2026-09-20 12:42:25 +0530

    Time: 8 ms (56.18%) | Memory: 19.8 MB (10.18%) - LeetSync

--- BackTracking/39-combination-sum/combination-sum.py
+++ BackTracking/39-combination-sum/combination-sum.py
@@ -13,4 +13,25 @@ class Solution:
                 sol.pop()  # Undo the choice
         backtrack(0, 0)  # Start from index 0 with sum 0
         return ans  # Return all valid combinations
-        
+
+"""
+For candidates = [2, 3, 6, 7], target = 7:
+
+Choose 2 → sol = [2], total = 2
+    Choose 2 again → sol = [2, 2], total = 4
+        Choose 2 again → total = 6
+            Choose 2 → total = 8, too large, so return.
+        Undo and choose 3 → sol = [2, 2, 3], total = 7 → save it.
+    Backtrack and try other choices.
+Choose 7 directly → sol = [7], total = 7 → save it.
+
+algo :
+Start with an empty combination sol = [] and total = 0.
+Choose a candidate and add it to sol.
+Add its value to total.
+If total == target, save the combination.
+If total > target, stop that path.
+Call backtrack(i, ...) using the same index i, because we can reuse the number.
+Remove the last chosen number using sol.pop().
+Try the next candidate.
+"""
