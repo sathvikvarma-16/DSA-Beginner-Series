24af1ca76473a84e266cc9a2b9c5a1f2e644f896
Konduru Sathvik Varma <sathvikvarma1216@gmail.com>
2026-09-20 13:34:47 +0530

    Time: 9 ms (19.14%) | Memory: 19.4 MB (20.88%) - LeetSync

---
+++ Strings/13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {  # Store Roman symbols and their values
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        total = 0  # Store the final integer
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
+                total -= values[s[i]]  # Subtract if smaller value comes before larger
+            else:
+                total += values[s[i]]  # Otherwise, add the value
+
+        return total  # Return the integer
