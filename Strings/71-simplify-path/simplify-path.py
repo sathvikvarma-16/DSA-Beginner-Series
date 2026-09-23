00263a4314217d94863d37bbc220c3be24b7b01a
Konduru Sathvik Varma <sathvikvarma1216@gmail.com>
2026-09-23 11:56:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (66.11%) - LeetSync

--- Strings/71-simplify-path/simplify-path.py
+++ Strings/71-simplify-path/simplify-path.py
@@ -1,21 +1,13 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-
         result = []
-
         folders = path.split("/")
-
         for folder in folders:
-
             if folder == "" or folder == ".":
                 continue
-
             elif folder == "..":
-
                 if result:
                     result.pop()
-
             else:
                 result.append(folder)
-
         return "/" + "/".join(result)
