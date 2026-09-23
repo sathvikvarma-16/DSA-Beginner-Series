b289ce732c7781d0617a7b7721c7fbf4809920a9
Konduru Sathvik Varma <sathvikvarma1216@gmail.com>
2026-09-23 11:31:39 +0530

    Time: 139 ms (78.65%) | Memory: 23.5 MB (41.64%) - LeetSync

--- Strings/4157-reverse-words-with-same-vowel-count/reverse-words-with-same-vowel-count.py
+++ Strings/4157-reverse-words-with-same-vowel-count/reverse-words-with-same-vowel-count.py
@@ -2,19 +2,15 @@ class Solution:
     def reverseWords(self, s: str) -> str:
         words = s.split()
         vowels = "aeiou"
-
         count = 0
         for ch in words[0]:
             if ch in vowels:
                 count += 1
-
         for i in range(1, len(words)):
             vowel_count = 0
             for ch in words[i]:
                 if ch in vowels:
                     vowel_count += 1
-
             if vowel_count == count:
                 words[i] = words[i][::-1]
-
         return " ".join(words)
