6f8c33a4114c91192e2fae6561ed12b4c0586a03
Konduru Sathvik Varma <sathvikvarma1216@gmail.com>
2026-09-23 10:32:16 +0530

    Time: 267 ms (66.67%) | Memory: 23 MB (78.72%) - LeetSync

--- Strings/952-word-subsets/word-subsets.py
+++ Strings/952-word-subsets/word-subsets.py
@@ -5,26 +5,19 @@ class Solution:
             freq = [0] * 26  # Count letters in this word
             for ch in word: 
                 freq[ord(ch) - ord('a')] += 1
-
             for i in range(26):
                 required[i] = max(required[i], freq[i])  # Keep the highest requirement
-
         ans = []
-
         for word in words1:  # Check every word in words1
             freq = [0] * 26  # Count letters in this word
-
             for ch in word:
                 freq[ord(ch) - ord('a')] += 1
-
             common = True
-
             for i in range(26):
                 if freq[i] < required[i]:  # Not enough of a required letter
                     common = False
                     break
-
             if common:
                 ans.append(word)
-
         return ans
+        
